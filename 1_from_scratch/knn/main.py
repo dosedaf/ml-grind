36fb63ad48270357ad4dca924960b4cf69db6216
dosedaf <mash1o1o1o1@gmail.com>
2025-09-09 07:39:10 +0700

    feat(knn): add tiebreak and distance functions
    Implements a tiebreak function to resolve ties in k-NN.
    Adds manhattan and minkowski distance functions.
    Updates predict functions to use distance functions.

--- 1_from_scratch/knn/main.py
+++ 1_from_scratch/knn/main.py
@@ -9,7 +9,20 @@ points = {
 }
 
 new_point = [4, 3]
-new_points = [[4, 3], [3, 8], [7, 8]]
+new_points = [[4, 3], [3, 5], [7, 5]]
+
+
+def tiebreak(neighbors, tie_num):
+    seen = {}
+    for neighbor in neighbors:
+        seen[neighbor] = 0
+
+    for neighbor in neighbors:
+        seen[neighbor] += 1
+        if seen[neighbor] == tie_num:
+            return neighbor
+
+    return None
 
 
 def euclidean_distance(p, q):
@@ -32,12 +45,14 @@ class KNearestNeighbors:
     def fit(self, points):
         self.points = points
 
-    def predict_single(self, new_point):
+    def predict_single(self, new_point, distance_fn=euclidean_distance):
         distances = []
+        categories = []
 
         for category in self.points:
+            categories.append(category)
             for point in self.points[category]:  # red, blue
-                distance = euclidean_distance(point, new_point)
+                distance = distance_fn(point, new_point)
                 distances.append([distance, category])
 
         sorted_distances = sorted(distances, key=lambda x: x[0])
@@ -56,18 +71,22 @@ class KNearestNeighbors:
 
         tied = [label for label, c in counts.items() if c == max_count]
 
+        # print(Counter(neighbors).most_common(1)[0][1])
+
         if len(tied) > 1:
-            result = Counter(neighbors[:-1]).most_common(1)[0][0]
+            result = tiebreak(neighbors, Counter(neighbors).most_common(1)[0][1])
 
         return sorted_distances[self.k - 1], result
 
-    def predict(self, new_points):
+    def predict(self, new_points, distance_fn=euclidean_distance):
         results = []
+        kth_neighbors = []
         for p in new_points:
-            _, result = self.predict_single(p)
+            kth_neighbor, result = self.predict_single(p, distance_fn=distance_fn)
             results.append(result)
+            kth_neighbors.append(kth_neighbor)
 
-        return results
+        return kth_neighbors, results
 
 
 def plotable(points, new_points):
@@ -91,7 +110,7 @@ def plotable(points, new_points):
 
 x, y, colors = plotable(points, new_points)
 
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(15, 15))
 plt.scatter(x, y, c=colors)
 
 plt.xlabel("x")
@@ -100,18 +119,27 @@ plt.ylabel("y")
 clf = KNearestNeighbors(k=4)
 clf.fit(points)
 
-kth_neighbor, result = clf.predict_single(new_point)
+kth_neighbor, result = clf.predict_single(new_point, distance_fn=minkowski_distance)
 
 radius = kth_neighbor[0]
 
-results = clf.predict(new_points)
+kth_neighbors, results = clf.predict(new_points, distance_fn=manhattan_distance)
 
 print(f"result: {result}")
 print(f"results: {results}")
 
-a = new_point[0] + radius * np.cos(np.linspace(0, 2 * np.pi, 200))
-b = new_point[1] + radius * np.sin(np.linspace(0, 2 * np.pi, 200))
 
-plt.plot(a, b, color="red")
+def plot_circle(new_points, kth_neighbors):
+    print(kth_neighbors[1])
+    for i in range(len(new_points)):
+        a = new_points[i][0] + kth_neighbors[i][0] * np.cos(
+            np.linspace(0, 2 * np.pi, 200)
+        )
+        b = new_points[i][1] + kth_neighbors[i][0] * np.sin(
+            np.linspace(0, 2 * np.pi, 200)
+        )
+        plt.plot(a, b, color="red")
 
+
+plot_circle(new_points, kth_neighbors)
 plt.show()
